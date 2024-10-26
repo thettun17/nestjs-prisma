36dcf270932f1aa907ffb5182e9c2783162e28f7
Thet Tun <thettun.loard17@gmail.com>
2024-10-26 16:33:26 +0700

    finished Product Module

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,47 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Prisma, Product } from '@prisma/client';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productService: ProductsService) {}
+  constructor(private readonly productService: ProductsService) {}
 
-    @Post()
-    async create(@Body() productCreateDto: Prisma.ProductCreateInput): Promise<Product> {
-        return this.productService.create(productCreateDto)
-    }
+  @Post()
+  async create(
+    @Body() productCreateDto: Prisma.ProductCreateInput,
+  ): Promise<Product> {
+    return this.productService.create(productCreateDto);
+  }
+
+  @Get()
+  async index(): Promise<Product[]> {
+    return this.productService.getAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<Product> {
+    return this.productService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() productUpdateDto: Prisma.ProductUpdateInput,
+  ): Promise<Product> {
+    return this.productService.update(id, productUpdateDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+	this.productService.remove(id);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,9 +5,55 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
+
   async create(productCreateDto: Prisma.ProductCreateInput): Promise<Product> {
     return await this.prisma.product.create({
       data: productCreateDto,
+    });
+  }
+
+  async getAll(): Promise<Product[]> {
+    return await this.prisma.product.findMany({
+      include: {
+        reviews: true,
+        description: true,
+        tags: true,
+      },
+    });
+  }
+
+  async findOne(id: number): Promise<Product> {
+    return await this.prisma.product.findFirst({
+      where: {
+        id: id,
+      },
+      include: {
+        reviews: true,
+        description: true,
+        tags: true,
+      },
+    });
+  }
+  async update(
+    id: number,
+    productUpdateDto: Prisma.ProductUpdateInput,
+  ): Promise<Product> {
+    return await this.prisma.product.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name: productUpdateDto.name,
+        price: productUpdateDto.price,
+      },
+    });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.prisma.product.delete({
+      where: {
+        id: id,
+      },
     });
   }
 }
